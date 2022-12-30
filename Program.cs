0e84e3f14dd07de1f29998e4fc723892637e9e7b
rawannn9 <121676425+rawannn9@users.noreply.github.com>
2022-12-30 15:33:53 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Please enter the start : ");
+int start = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter the end : ");
+int end = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = start; i <= end; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        if (i % j == 0)
+            sum++;
+    }
+
+    if (sum == 2)
+    {
+        Console.WriteLine(i);
+        sum = 0;
+
+    }
+
+    else
+    {
+        sum = 0;
+        continue;
+    }
+}
+
+
+
